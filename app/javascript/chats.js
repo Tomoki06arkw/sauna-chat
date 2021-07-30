3c31e5f2dabe880865bfd1244b0b5cb5bdac00ea
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-07-30 14:46:45 +0900

    JS導入

---
+++ app/javascript/chats.js
@@ -0,0 +1,19 @@
+$(function(){
+  $('.s_01 .accordion_one .accordion_header').on('click',function(){
+    $(this).next('.accordion_inner').slideToggle();
+    $(this).toggleClass("open");
+  });
+});
+
+$(function(){
+  const INDEX = "-visible";
+  const TIMEOUT = 2000;
+  const $target = $(".title");
+
+    setInterval(() => {
+      $target.addClass(INDEX);
+      setTimeout(() => {
+        $target.removeClass(INDEX);
+      }, TIMEOUT);
+    }, TIMEOUT * 2);
+});
