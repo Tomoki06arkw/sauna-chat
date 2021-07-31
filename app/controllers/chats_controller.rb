1267779fcd4e9abad6d443ace843a0ff14532c9a
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-07-31 09:31:30 +0900

    ログイン、ログアウト機能実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,14 @@
 class ChatsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+  end
+
+  private
+  
+  def move_to_index
+    unless user_singed_in?
+      redirect_to aciton: index
+    end
   end
 end
