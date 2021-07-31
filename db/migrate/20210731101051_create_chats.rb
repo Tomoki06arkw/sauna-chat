81ef71a45baadcbc21f4f24ccfab3d3de99651c7
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-07-31 19:15:50 +0900

    Chat Model実装

---
+++ db/migrate/20210731101051_create_chats.rb
@@ -0,0 +1,13 @@
+class CreateChats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chats do |t|
+      t.integer :area_id,           null: false
+      t.integer :price,             null: false
+      t.string  :sauna_name,        null: false
+      t.text    :description,       null: false
+      t.references :user,           foregin_key: true
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_232356) do
+ActiveRecord::Schema.define(version: 2021_07_31_101051) do
+
+  create_table "chats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "area_id", null: false
+    t.integer "price", null: false
+    t.string "sauna_name", null: false
+    t.text "description", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
