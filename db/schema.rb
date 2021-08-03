883cdc5247b44f8c00ff3ec87421f898e8bfb350
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-03 17:12:28 +0900

    ActionCableを使ったチャット機能実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_035257) do
+ActiveRecord::Schema.define(version: 2021_08_03_075058) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2021_08_02_035257) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chat_id"], name: "index_comments_on_chat_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
