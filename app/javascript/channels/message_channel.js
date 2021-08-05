acb272f2579c0b5206c9bc49ccf7ab89064ffa89
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-05 12:21:29 +0900

    通信変更。

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -14,6 +14,5 @@ consumer.subscriptions.create("MessageChannel", {
     const messages = document.getElementById('messages');
     const newMessage = document.getElementById('message_text');
     messages.insertAdjacentHTML('afterbegin', html);
-    newMessage.value='';
   }
 });
