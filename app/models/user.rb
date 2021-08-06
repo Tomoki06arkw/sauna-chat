efdb61d0bf73ce2e13798e3c004068875d77cf5a
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-06 17:30:14 +0900

    ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,15 @@ class User < ApplicationRecord
         validates :profile
         validates :experience
   end
+
+  def self.guest
+    find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+      user.nickname = 'ゲスト'
+      user.profile = 'サウナー'
+      user.experience = '最近始めました'
+    end
+  end
   
 end
