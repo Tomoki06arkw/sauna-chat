7e419182645a88c93d655efdd5ad687039f4772b
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-11 17:25:14 +0900

    JS追加実装

--- app/javascript/chats.js
+++ app/javascript/chats.js
@@ -49,3 +49,15 @@ $(function(){
     $('html, body').animate({scrollTop:0}, 300)
   }); 
 });
+
+$(function() {
+  $('.more-btn').on('click', function() {
+    if( $(this).children().is('.open') ) {
+      $(this).html('<p class="close">閉じる</p>').addClass('close-btn');
+      $(this).parent().removeClass('slide-up').addClass('slide-down');
+    } else {
+      $(this).html('<p class="open">もっと見る</p>').removeClass('close-btn');
+      $(this).parent().removeClass('slide-down').addClass('slide-up');
+    }
+  });
+});
