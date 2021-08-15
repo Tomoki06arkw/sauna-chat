efd3d07abb378c1b247cef900a46391aedd9ef43
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-15 18:40:45 +0900

    英語に変換。

--- app/javascript/chats.js
+++ app/javascript/chats.js
@@ -52,7 +52,7 @@ $(function(){
 $(function() {
   $('.more-btn').on('click', function() {
     if( $(this).children().is('.open') ) {
-      $(this).html('<p class="close">閉じる</p>').addClass('close-btn');
+      $(this).html('<p class="close">Close</p>').addClass('close-btn');
       $(this).parent().removeClass('slide-up').addClass('slide-down');
     } else {
       $(this).html('<p class="open">もっと見る</p>').removeClass('close-btn');
