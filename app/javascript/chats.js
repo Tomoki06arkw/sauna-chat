4a0ed7cc7f9f53b7b08b605c16064bd30a7b975e
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-17 09:59:42 +0900

    JS修正。

--- app/javascript/chats.js
+++ app/javascript/chats.js
@@ -55,7 +55,7 @@ $(function() {
       $(this).html('<p class="close">Close</p>').addClass('close-btn');
       $(this).parent().removeClass('slide-up').addClass('slide-down');
     } else {
-      $(this).html('<p class="open">もっと見る</p>').removeClass('close-btn');
+      $(this).html('<p class="open">Show more</p>').removeClass('close-btn');
       $(this).parent().removeClass('slide-down').addClass('slide-up');
     }
   });
