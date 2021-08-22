fc7312cc40fc1956f7869bc524d932fae888f39e
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-22 09:21:54 +0900

    いいね機能導入。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action move_to_index 
+
   def show
     @user = User.find(params[:id])
     @chats = Kaminari.paginate_array(Chat.includes(:user).reverse_order).page(params[:page]).per(4)
